63c944a8da1e6e64b334d57f311f46bb6fb02b07
Vladimir Shisterov <v.shisterov76u@gmail.com>
2025-11-20 17:54:30 +0300

    fix inline kb

--- core/refuel_bot/keyboards/refuel_method_keyboard.py
+++ core/refuel_bot/keyboards/refuel_method_keyboard.py
@@ -6,11 +6,9 @@ class RefuelMethodKeyboard:
     def get_inline(self):
         keyboard = [
             [
-                [
-                    InlineKeyboardButton("ТГ-бот", callback_data="refuel_method:tg_bot"),
-                    InlineKeyboardButton("Карта", callback_data="refuel_method:fuel_card")
-                ],
-                InlineKeyboardButton("Топливозаправщик", callback_data="refuel_method:truck")
+                InlineKeyboardButton("ТГ-бот", callback_data="refuel_method:tg_bot"),
+                InlineKeyboardButton("Карта", callback_data="refuel_method:fuel_card")                
             ],
+            InlineKeyboardButton("Топливозаправщик", callback_data="refuel_method:truck")
         ]
         return InlineKeyboardMarkup(keyboard)
